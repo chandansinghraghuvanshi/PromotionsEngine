3ce238daca0f08e409c7029799fb33e53b6f89da
ChandanSinghRaghuvanshi <chandansingh4@gmail.com>
2020-09-14 13:55:59 +0530

    Mode for Cart added.

---
+++ PromotionsEngine/PromotionsEngine/Models/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionsEngine.Models
+{
+    class Cart
+    {
+        private List<StockKeepingUnit> stockKeepingUnits = new List<StockKeepingUnit>();
+
+        public List<StockKeepingUnit> StockKeepingUnits
+        {
+            get
+            {
+                return stockKeepingUnits;
+            }
+            set
+            {
+                stockKeepingUnits = value;
+            }
+        }
+    }
+}
