8e7bd800049cc7a76664130b68fbeddd2b3c920d
ChandanSinghRaghuvanshi <chandansingh4@gmail.com>
2020-09-15 02:34:16 +0530

    Promotion Model added.

---
+++ PromotionsEngine/PromotionsEngine/Models/Promotion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionsEngine.Models
+{
+    class Promotion
+    {
+        private string skuRequired;
+        private int numberOfSkuRequired;
+
+        public string SkuRequired
+        {
+            get
+            {
+                return skuRequired;
+            }
+            set
+            {
+                skuRequired = value;
+            }
+        }
+
+        public int NumberOfSkuRequired
+        {
+            get
+            {
+                return numberOfSkuRequired;
+            }
+            set
+            {
+                numberOfSkuRequired = value;
+            }
+        }
+    }
+}
--- PromotionsEngine/PromotionsEngine/Program.cs
+++ PromotionsEngine/PromotionsEngine/Program.cs
@@ -13,6 +13,11 @@ namespace PromotionsEngine
 
             List<string> skuIds = new List<string>() { "A", "B", "C", "D", "A", "A", "A", "A", "B", "B", "B", "C", "C", "C", "A", "D", "D", "C", "C", "B", "B" };
 
+            List<Promotion> promotions = new List<Promotion>() {
+                { new Promotion { SkuRequired = "A", NumberOfSkuRequired = 3 } },
+                { new Promotion { SkuRequired = "B", NumberOfSkuRequired = 2 } },
+                { new Promotion { SkuRequired = "C, D", NumberOfSkuRequired = 1 } }
+            };
             Dictionary<string, int> skuDictionary = SkuUtilities.CreateSkuDictionary(skuIds);
 
             Cart cart = SkuUtilities.GetCart(skuDictionary, skuPriceDictionary);
